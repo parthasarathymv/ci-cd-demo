312ba9397c7a8ab71d61dcb237b0644cdc955249
Parthasarathy Murali <partha@juhomi.com>
2025-11-24 23:20:42 +0530

    staging

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send(`
     <h1>Welcome to CI/CD Demo</h1>
-    <p>This page is rendered from the latest deployment from staging test!</p>
+    <p>This page is rendered from the latest deployment from staging test one!</p>
   `);
 });
 
