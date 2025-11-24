045bad17a273b4e10686a70d06bbc699e7bea851
Parthasarathy Murali <partha@juhomi.com>
2025-11-24 23:14:20 +0530

    staging

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send(`
     <h1>Welcome to CI/CD Demo</h1>
-    <p>This page is rendered from the latest deployment from staging!</p>
+    <p>This page is rendered from the latest deployment from staging test!</p>
   `);
 });
 
