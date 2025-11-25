e094f55e2c13ccd572c2b26fbeda4df8a1214cf0
Parthasarathy Murali <partha@juhomi.com>
2025-11-25 16:53:49 +0530

    build test

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const host = "0.0.0.0.";
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Welcome to CI/CD Demo</h1>
+    <p>This page is rendered from main branch!</p>
+  `);
+});
+
+app.listen(port, host, () => {
+  console.log(`Server running on http://${host}:${port}`);
+});
