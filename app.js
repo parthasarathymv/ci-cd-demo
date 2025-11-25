7ce447067f8293bf2ac36dab3de0833334c659f9
Parthasarathy Murali <partha@juhomi.com>
2025-11-25 10:36:18 +0530

    build test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const host = "0.0.0.0.";
 app.get('/', (req, res) => {
   res.send(`
     <h1>Welcome to CI/CD Demo</h1>
-    <p>This page is rendered from the latest deployment from staging test!</p>
+    <p>This page is rendered from the latest deployment from staging test 123!</p>
   `);
 });
 
