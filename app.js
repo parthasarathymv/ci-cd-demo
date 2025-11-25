164dba7d2b754d261963a60109e1be35aca98a4b
Parthasarathy Murali <partha@juhomi.com>
2025-11-25 10:20:36 +0530

    build test

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const host = "0.0.0.0.";
 
 app.get('/', (req, res) => {
   res.send(`
     <h1>Welcome to CI/CD Demo</h1>
-    <p>This page is rendered from the latest deployment from staging test one!</p>
+    <p>This page is rendered from the latest deployment from staging test!</p>
   `);
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, host, () => {
+  console.log(`Server running on http://${host}:${port}`);
 });
