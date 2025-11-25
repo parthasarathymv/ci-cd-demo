6b5c0498d2c8ab4fb85bdb9d4bb85b0a3d7ce4d9
Parthasarathy Murali <partha@juhomi.com>
2025-11-25 17:21:42 +0530

    build test

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const host = "0.0.0.0.";
+const host = "0.0.0.0";
 
 app.get('/', (req, res) => {
   res.send(`
     <h1>Welcome to CI/CD Demo</h1>
-    <p>This page is rendered from main branch!</p>
+    <p>This page is rendered from main branch 123!</p>
   `);
 });
 
